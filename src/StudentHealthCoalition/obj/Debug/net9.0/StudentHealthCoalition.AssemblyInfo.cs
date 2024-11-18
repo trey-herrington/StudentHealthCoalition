a35c31ba2f4cd2a98e9030320f2a815cb46b2556
Trey Herrington <trey.herrington00@gmail.com>
2024-11-18 02:59:44 -0600

    changes

--- src/StudentHealthCoalition/obj/Debug/net9.0/StudentHealthCoalition.AssemblyInfo.cs
+++ src/StudentHealthCoalition/obj/Debug/net9.0/StudentHealthCoalition.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentHealthCoalition")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03cb545af12fcd22826a6e06e8bfa743b2a6f865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f78017e68b4b23696e74de2dfdbc8616eb15adaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentHealthCoalition")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentHealthCoalition")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
